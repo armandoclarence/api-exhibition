63bbadddc4b986424bc11203399bd8569a848d68
armandoclarence <aclarence200@gmail.com>
2024-03-08 19:35:33 +0700

    stall, token valid

---
+++ table.sql
@@ -0,0 +1,20 @@
+CREATE TABLE registration (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  firstname VARCHAR(255),
+  lastname VARCHAR(255),
+  email VARCHAR(255),
+  mobile INT,
+  password VARCHAR(255),
+  user_type_id VARCHAR(255)
+);
+
+CREATE TABLE stall (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  firstname VARCHAR(255),
+  lastname VARCHAR(255),
+  email VARCHAR(255),
+  mobile INT,
+  password VARCHAR(255),
+  user_type_id VARCHAR(255)
+);
+
